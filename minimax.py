e5dd40782cd4ae96fdb61babc84f3863eca9c342
lcubelongren <59025855+lcubelongren@users.noreply.github.com>
2020-05-31 02:35:25 +0200

    Add files via upload
    Finished the basic minimax implementation

--- minimax.py
+++ minimax.py
@@ -8,48 +8,92 @@ this is a raw minimax agent without alpha-beta pruning
 """
 
 
-def heuristic(
+def heuristic_basic(
         board: np.ndarray, player: BoardPiece
 ) -> np.int:
     """
     using similar code as what is used to check for a connect_n
     we can create a heuristic that assigns a higher value to a board
     where a player has more adjacent pieces and the highest value
-    or when the player has a winning board
+    for when the player has a winning board
+    """
+    """
+    this is the starting part of a better heuristic. 
+    points that could be made better are that depth is not taken into account,
+    which then makes the minimax agent not know that it could be losing if it 
+    does not play a certain move, and just sees more adjecent pieces as better
     """
     from agents.connectn.common import CONNECT_N
 
     rows, cols = board.shape
     rows_edge = rows - CONNECT_N + 1
     cols_edge = cols - CONNECT_N + 1
-    rating = 0  # starting rating
-    rating_base = int(1e6)  # base rating for each check
+    rating = 0  # starting rating (no connecting pieces)
+    degree = 5  # weighting for the number connected (exponential)
 
     for i in range(rows):  # count vertical pieces
         for j in range(cols_edge):
-            for k in range(CONNECT_N-1):
+            for k in range(CONNECT_N-1):  # count up to a winning board
                 if np.all(board[i, j:j+CONNECT_N-k] == player):  # connected_n-k
-                    rating += int(rating_base - k*rating_base/CONNECT_N)
+                    rating += CONNECT_N ** degree
 
     for i in range(rows_edge):  # count horizontal pieces
         for j in range(cols):
-            for k in range(CONNECT_N-1):
+            for k in range(CONNECT_N-1):  # count up to a winning board
                 if np.all(board[i:i+CONNECT_N-k, j] == player):  # connected_n-k
-                    rating += int(rating_base - k*rating_base/CONNECT_N)
+                    rating += CONNECT_N ** degree
 
     for i in range(rows_edge):  # count diagonal pieces
         for j in range(cols_edge):
-            for k in range(CONNECT_N-1):
+            for k in range(CONNECT_N-1):  # count up to a winning board
                 block = board[i:i+CONNECT_N-k, j:j+CONNECT_N-k]  # connected_n-k
                 if np.all(np.diag(block) == player):
-                    rating += int(rating_base - k*rating_base/CONNECT_N)
+                    rating += CONNECT_N ** degree
                 if np.all(np.diag(block[::-1, :]) == player):
-                    rating += int(rating_base - k*rating_base/CONNECT_N)
+                    rating += CONNECT_N ** degree
     return rating
 
 
+def heuristic_better(
+        board: np.ndarray, player: BoardPiece,
+) -> np.int:
+    """
+    taking our basic heuristic, let's make it a bit better
+    """
+    from agents.connectn.common import CONNECT_N
+
+    rows, cols = board.shape
+    rows_edge = rows - CONNECT_N + 1
+    cols_edge = cols - CONNECT_N + 1
+    rating = 0  # starting rating (no connecting pieces)
+    degree = 5  # weighting for the number connected (exponential)
+
+    for i in range(rows):  # count vertical pieces
+        for j in range(cols_edge):
+            for k in range(CONNECT_N-1):  # count up to a winning board
+                if np.all(board[i, j:j+CONNECT_N-k] == player):  # connected_n-k
+                    rating += CONNECT_N ** degree
+
+    for i in range(rows_edge):  # count horizontal pieces
+        for j in range(cols):
+            for k in range(CONNECT_N-1):  # count up to a winning board
+                if np.all(board[i:i+CONNECT_N-k, j] == player):  # connected_n-k
+                    rating += CONNECT_N ** degree
+
+    for i in range(rows_edge):  # count diagonal pieces
+        for j in range(cols_edge):
+            for k in range(CONNECT_N-1):  # count up to a winning board
+                block = board[i:i+CONNECT_N-k, j:j+CONNECT_N-k]  # connected_n-k
+                if np.all(np.diag(block) == player):
+                    rating += CONNECT_N ** degree
+                if np.all(np.diag(block[::-1, :]) == player):
+                    rating += CONNECT_N ** degree
+    # return rating
+    return NotImplemented
+
+
 def minimax(
-    board: np.ndarray, player: BoardPiece, depth: np.int, max_player: bool
+    board: np.ndarray, player: BoardPiece, depth: np.int, max_player: bool, heuristic
 ) -> Tuple[PlayerAction, Optional[SavedState]]:
     """
     implement the minimax move generator (without pruning for now)
@@ -59,9 +103,14 @@ def minimax(
     from agents.connectn.common import apply_player_action
     from agents.connectn.common import pretty_print_board
 
+    # other_player variable is used to alternate player pieces as depth increases
+    if player == 1:
+        other_player = 2
+    else:
+        other_player = 1
+
     # should define which are columns are free here to reduce computation time
     node_num = np.shape(board)[1]  # number of moves initially allowed
-    depth_result = np.zeros(node_num)  # empty array for result
 
     # pretty_print_board(board)
 
@@ -69,19 +118,25 @@ def minimax(
         return heuristic(board, player)
 
     if max_player:  # maximizing player level
-        value = int(-1e6)  # negative infinity
+        node_value = np.full(node_num, int(-1e10))  # empty array for node values, negative inf
         for node in range(node_num):  # for each child node, maximize
             board_copy = apply_player_action(board, node, player, copy=True)
-            comparison = minimax(board_copy, player, depth-1, False)
-            depth_result[node] = np.max([value, comparison])
+            comparison = minimax(board_copy, player, depth-1, False, heuristic)
+            if np.min(comparison) > node_value[node]:
+                node_value[node] = np.min(comparison)
+        return node_value
 
     else:  # minimizing player level
-        value = int(1e6)  # positive infinity
+        node_value = np.full(node_num, int(1e10))  # empty array for node values, positive inf
         for node in range(node_num):  # for each child node, minimize
-            board_copy = apply_player_action(board, node, player, copy=True)
-            comparison = minimax(board_copy, player, depth-1, True)
-            depth_result[node] = np.min([value, comparison])
-    return depth_result
+            #board_copy = apply_player_action(board, node, player, copy=True)
+            board_copy = apply_player_action(board, node, other_player, copy=True)
+            #comparison = minimax(board_copy, player, depth-1, True, heuristic)
+            comparison = minimax(board_copy, other_player, depth-1, True, heuristic)
+            if np.max(comparison) < node_value[node]:
+                node_value[node] = np.max(comparison)
+        return node_value
+
 
 
 def generate_move_minimax(
@@ -90,7 +145,13 @@ def generate_move_minimax(
     """
     generate the move for the agent from the minimax function using our heuristic
     """
-    depth = 1
-    action_set = minimax(board, player, depth, True)
-    action = np.argmax(action_set)  # maximize the best move
+    depth = 4
+    heuristic = heuristic_basic  # choose which heuristic to use
+    #heuristic = heuristic_better
+    action_set = minimax(board, player, depth, True, heuristic=heuristic)
+    print(action_set)
+    if len(set(action_set)) == 1:  # if all are the same
+        action = 3  # put piece in the center
+    else:
+        action = np.argmax(action_set)  # maximize the best move
     return action, saved_state
