cb8f694a22c024f6b4de038c6deef2c1cc25a3ee
lcubelongren <59025855+lcubelongren@users.noreply.github.com>
2020-07-27 17:46:25 +0200

    Add files via upload

--- myMCTS/agent_MCTS/MCTS.py
+++ myMCTS/agent_MCTS/MCTS.py
@@ -1,22 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import random
 import time
 from typing import Optional, Tuple
-from game_CONNECTN.connectn.common import PlayerAction, BoardPiece, SavedState, check_end_state, initialize_game_state
-from game_CONNECTN.connectn.common import apply_player_action, NO_PLAYER, PLAYER1, PLAYER2, GameState
-from math import log, sqrt
+from game_CONNECTN.connectn.common import PlayerAction, BoardPiece, SavedState, NO_PLAYER, PLAYER1, PLAYER2
+from game_CONNECTN.connectn.common import apply_player_action, check_end_state, GameState
 
 
-def allowed_moves(board):
-    if np.ndim(board) == 2:
-        pass
-    elif np.ndim(board) == 3:
-        board = board[-1]
-    elif np.ndim(board) == 0 or np.ndim(board) == 1:
-        return np.arange(0, 7, 1)
-    else:
-        print('broken allowed_moves input dimensions')
+# functions to be used within the MCTS Class:
+
+def allowed_moves(board: np.ndarray):
+    """
+    input ->
+    board: the array for the given board state
+    output ->
+    allowed: the allowed moves from the input board state
+    """
+    # if np.ndim(board) == 2:
+    #     pass
+    # elif np.ndim(board) == 3:
+    #     board = board[-1]
+    # else:
+    #     print('broken allowed_moves input dimensions')
     allowed = list([])
     for i in range(np.shape(board)[1]):
         if board[-1, i] == NO_PLAYER:
@@ -24,7 +28,13 @@ def allowed_moves(board):
     return allowed
 
 
-def grab_player(board):
+def grab_player(board: np.ndarray or Tuple):
+    """
+    input ->
+    board: the array for the given board state
+    output ->
+    allowed: the player who's turn it is to make a move
+    """
     num_1 = len(np.where(board == PLAYER1)[0])
     num_2 = len(np.where(board == PLAYER2)[0])
     if num_2 >= num_1:
@@ -35,27 +45,39 @@ def grab_player(board):
         print('broken grab_player inequality')
 
 
-def totuple(array):
+def totuple(array: np.ndarray):
+    """
+    input ->
+    array: any numpy array
+    output ->
+    type(array) == Tuple
+    """
     return tuple(map(tuple, array))
 
 
+# the MCTS network Class:
+
 class MCTSnet(object):
+
+    """the entire Monte Carlo Tree Search network"""
 
     def __init__(self, board, state=None, debug=False, **kwargs):
         """
         kwargs ->
         calc_time: calculation length [s]
         move_max: maximum number of moves
-        C: configuration parameter
+        C: exploration/exploitation parameter
         """
         super().__init__()
         # initialize the game board, player, and states
-        self.board = board  # the current board state
+        self.board = totuple(board)  # the current board state
         self.player = grab_player(self.board)  # the player to play
-        self.states = [board]
-        if state is not None:
-            for s in state:
-                self.states.append(s)
+        self.states = []
+        self.new_states = set()
+        # # the following add SaveState ability
+        # if state is not None:
+        #     for s in state:
+        #         self.states.append(s)
         # set for how long to run a calculation
         self.calc_time = kwargs.get('calc_time')
         # set a maximum number of moves and track depth
@@ -66,16 +88,21 @@ class MCTSnet(object):
         self.plays = {}
         # define the exploration parameter
         self.c = kwargs.get('c')
+        # debugging and recording random stuff
         self.debug = debug
+        self.random_count = 0
+        self.not_random_count = 0
 
     def best_move(self):
-        # this function is what is ran in the
-        # generate move function outside of the class
-        # determine the best move and return it
-        state = self.board
+        """
+        this function is what is ran in the
+        generate move function outside of the class
+        determine the best move and return it
+        """
+        self.states.append(self.board)  # start with the current board
+        state = self.states[-1]
         player = grab_player(state)
-        allowed = allowed_moves(state)
-        # print(allowed)
+        allowed = allowed_moves(np.array(state))
         # ensure a choice is to be made
         if len(allowed) == 1:
             return allowed[0]
@@ -91,112 +118,138 @@ class MCTSnet(object):
         time_elap = time.time() - start
         print('simulation number: {0}, time elapsed: {1}'.format(sim_num, time_elap))
 
+        # print(len(self.wins))
+        # print(len(self.plays))
+
         # choose the best move
-        poss_moves = [(n, totuple(apply_player_action(np.array(state[0]), n, player, copy=True))) for n in allowed]
+        poss_moves = [(n, totuple(apply_player_action(np.array(self.board), n, player, copy=True))) for n in allowed]
         win_ratio = ((self.wins.get((player, s), 0) / self.plays.get((player, s), 1), p) for p, s in poss_moves)
+        # maximize the number of visits
         to_maximize = np.transpose(np.array(list(win_ratio)))
-        move = to_maximize[1, np.argmax(to_maximize[0])]
+        move = np.argmax(to_maximize[0])
 
         # debugging statistics ---
         if self.debug:
-            for x in sorted(
-                ((100 * self.wins.get((player, s), 0) / self.plays.get((player, s), 1),
-                 self.wins.get((player, s), 0), self.plays.get((player, s), 0), p+1)
-                    for p, s in poss_moves),
-                reverse=True
-            ):
-                print("{3}: {0:.2f}% ({1} / {2})".format(*x))
-            print('Depth maximum: {}'.format(self.depth_max))
-            # print('Possible moves: {}'.format(allowed))
+            for data in sorted(
+                (((self.wins.get((player, s), 0) * 100) / self.plays.get((player, s), 1),  # 0. win percentage
+                 self.wins.get((player, s), 0),  # 1. win number
+                  self.plays.get((player, s), 0),  # 2. play number
+                  p + 1)  # 3. play
+                    for p, s in poss_moves), reverse=True):
+                if move == data[3]-1:  # point to the chosen value
+                    print("{3}: ({1} / {2}) = {0:.2f}% <--".format(*data))
+                else:
+                    print("{3}: ({1} / {2}) = {0:.2f}%".format(*data))
+            print('Depth maximum     : {}'.format(self.depth_max))
+            # print('Possible moves  : {}'.format(allowed))
+            print('Random choice #   : {}'.format(self.random_count))
+            print('Educated choice # : {}'.format(self.not_random_count))
+            print('State array shape : {}'.format(np.shape(self.states)))
 
         return move
 
     def run_sim(self):
-        # add the current ratio numbers
-        plays = self.plays
-        wins = self.wins
-        # ready to check if current state is new
-        visited_states = set()
+        """
+        this function runs until the maximum moves have
+        been reached or the board state is a win or draw.
+        in the process, depending upon if a player/state combo
+        is new, the agent will go between exploration and exploitation.
+        UCB1 is used to determine the ratio between them.
+        """
+        # clear the set to record the states
+        # that have been visited this simulation
+        self.new_states.clear()
         # play a 'random' game from current position
-        # and update the statistic tables with the result
-        states_copy = self.states
-        state = states_copy[-1]
-        # grab player
+        # that increasingly gains more information over simulations
+        # and update the statistic tables with each result
+        states_copy = self.states  # create copy to be used for this simulation
+        state = self.states[-1]  # the node state that expansion has stopped at
+        # grab the current player
         player = grab_player(state)
 
         # run a simulation
+        count = 0
         expansion = True
         has_won = False
-        for t in range(self.move_max):
+        has_drawn = False
+        # --- SIMULATION PHASE is contained here
+        while count < self.move_max and not (has_won or has_drawn):
+            # print(count)
+            count += 1
+            # --- SELECTION PHASE
             # define the legal moves for current position
-            allowed = allowed_moves(np.array(states_copy))
+            allowed = allowed_moves(np.array(state))
+            # create all the possible next moves -> (play, board)
             poss_moves = [(n, totuple(apply_player_action(np.array(state), n, player, copy=True))) for n in allowed]
+            # print(poss_moves)
             # check if we know the statistics for the possible moves
             # here we use the UCB1 algorithm
-            if len(poss_moves) < 1:
-                break
-            elif all(plays.get((player, s)) for p, s in poss_moves):
-                # print(np.shape(poss_moves))
+            # if len(poss_moves) < 1:
+            #    break
+            if all(self.plays.get((player, s)) for p, s in poss_moves):
                 # If we have stats on all of the legal moves here, use them.
-                log_sum = log(sum(plays[(player, s)] for p, s in poss_moves))
-                _, move, state = max(
-                    ((wins[(player, s)] / plays[(player, s)]) +
-                     self.c * sqrt(log_sum / plays[(player, s)]), p, s)
-                    for p, s in poss_moves
-                )
+                self.not_random_count += 1
+                for p, s in poss_moves:
+                    win_ratio = self.wins[(player, s)] / self.plays[(player, s)]
+                    log_sum = np.log(np.sum(self.plays[(player, s)]))
+                    exploration_term = self.c * np.sqrt(log_sum / self.plays[(player, s)])
+                    out, move, state = np.max(win_ratio + exploration_term, p, np.array(s))
             # otherwise just make a random decision
             else:
+                self.random_count += 1
                 move, state = random.choice(poss_moves)
 
-            # update the states array
-            # print(xp.shape(states_copy), np.shape(state))
-            # states_copy = np.append(states_copy, np.array(state), axis=1)
-            states_copy.append(np.array(state))
-            # print(states_copy)
-            # -------
+            # update the copied states array
+            states_copy.append(state)
+
+            # if a new node and expanding, stop expanding
+            # and add a starting dictionary entry for the move
             if (player, state) not in self.plays and expansion:
+                # --- EXPANSION PHASE (or end of)
+                # initialize the new node dictionary values
                 expansion = False
-                plays[(player, state)] = 0
-                wins[(player, state)] = 0
-                if t > self.depth_max:
-                    self.depth_max = t
+                self.plays[(player, state)] = 0
+                self.wins[(player, state)] = 0
+                if count > self.depth_max:
+                    self.depth_max = count
 
-            # only add the new player state combos
-            visited_states.add((player, state))
+            # add only the new player state combos
+            self.new_states.add((player, state))
+
             # update the player
-            player = grab_player(state)
-            # check if the state is winning
-            # print(np.shape(np.array(states_copy)))
-            for s in np.array(states_copy):
-                if check_end_state(s, player) == GameState.IS_WIN:
-                    has_won = True
-                    break
-                else:
-                    has_won = False
-                    pass
+            player = grab_player(np.array(state))
 
-        # back-propagation
+            # check if the state is winning
+            # if so, it breaks out of the while loop
+            # print(check_end_state(np.array(state), player))
+            if check_end_state(np.array(state), player) == GameState.IS_WIN:
+                has_won = True
+            elif check_end_state(np.array(state), player) == GameState.IS_DRAW:
+                has_drawn = True
+
         # update plays and wins from our set
-        for player, state in visited_states:
+        # --- BACKPROPAGATION PHASE
+        for player, state in self.new_states:
+            # print((player, state))
             if (player, state) not in self.plays:
+                # skip the given play if state has not been tried
                 continue
-            plays[(player, state)] += 1
-            if player == has_won:
-                wins[(player, state)] += 1
+            self.plays[(player, state)] += 1
+            if has_won:
+                self.wins[(player, state)] += 1
 
 
 def generate_move_mcts(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState]
 ) -> Tuple[PlayerAction, Optional[SavedState]]:
-    """
-    generate the move for the agent from the MCTS class
-    """
-    calc_time = 1
-    move_max = int(6*7)
-    c = 2
+
+    """generate the move for the agent using the MCTS network"""
+
+    calc_time = 0.08
+    move_max = 100
+    c = np.sqrt(2)
 
     mcts = MCTSnet(board, state=None, calc_time=calc_time, move_max=move_max, c=c, debug=True)
     action = mcts.best_move()
-    print(np.shape(mcts.states))
 
     # hist = SavedState(first_state=[])
 
